0154bf920176ff703b84e93d79611febcf8d0491
Rivu Khoda <rivu.khoda@gmail.com>
2015-08-04 01:31:35 -0400

    Refactor: script name has changed

---
+++ note_counter.py
@@ -0,0 +1,2 @@
+note_sequence = raw_input('Please enter a sequence of notes :\n')
+print "Number of notes: {}".format(len(note_sequence))
--- pitch_counter.py
+++
@@ -1,2 +0,0 @@
-pitch_sequence = raw_input('Please enter a sequence of pitches:\n')
-print "Number of pitches: {}".format(len(pitch_sequence))
