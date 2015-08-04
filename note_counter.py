2d68ad165c66e8bbc79884adfcb0957e58b7b9e6
Rivu Khoda <rivu.khoda@gmail.com>
2015-08-04 15:27:22 -0400

    Refactor: fixed space

--- note_counter.py
+++ note_counter.py
@@ -1,2 +1,2 @@
-note_sequence = raw_input('Please enter a sequence of notes :\n')
+note_sequence = raw_input('Please enter a sequence of notes:\n')
 print "Number of notes: {}".format(len(note_sequence))
