28e6437546e7fa22e074f34bc82e25e16ca4c036
Rivu Khoda <rivu.khoda@gmail.com>
2015-08-05 01:03:54 -0400

    New: Volpiano related functions are in single module

--- volpiano.py
+++ volpiano.py
@@ -1,32 +1,7 @@
 import csv
 import os
 
-<<<<<<< HEAD
-script_dir = os.path.dirname(__file__)
-file_path = raw_input('Enter path to csv file to be queried:\n')
-abs_file_path = os.path.join(script_dir, file_path)
 
-with open(abs_file_path) as csvfile:
-    reader = csv.DictReader(csvfile)
-    page = raw_input('Enter desired Folio number:\n')
-
-    previous = None
-    is_folio_found = False
-
-    for row in reader:
-        if row['Folio'] == page:
-            is_folio_found = True
-            if previous['Folio'] != page:
-                print previous['Folio'], previous['Volpiano']
-                print '\n'
-            print row['Folio'], row['Volpiano']
-            print '\n'
-        else:
-            if is_folio_found:
-                print row['Folio'], row['Volpiano']
-                is_folio_found = False
-        previous = row
-=======
 def query():
     script_dir = os.path.dirname(__file__)
     file_path = raw_input('Enter path to csv file to be queried:\n')
@@ -75,5 +50,5 @@ def convert():
         else:
             new_sequence += volpiano_map.get(s)
 
-    print new_sequence
->>>>>>> parent of 0d69d4b... Fixed: modified .gitignore to ignore .pyc files
+    return new_sequence
+
