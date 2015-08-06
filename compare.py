01678b0d7d5e3f7fed622d7249d2a71c50d2d8d2
Rivu Khoda <rivu.khoda@gmail.com>
2015-08-06 02:27:53 -0400

    New: Outputs note sequence to compare for multiple mei files

--- compare.py
+++ compare.py
@@ -1,26 +1,41 @@
 import volpiano
 import mei
 import note
+import os
 from datetime import datetime
-
-
-csvfile = "sal-data.csv"
-folio = "003r"
-notes = volpiano.query("../data/" + csvfile, folio)
-converted_notes = volpiano.convert(notes)
-symbols = ['1', '3', '7', '4', '-', 'i']
-extracted_notes = volpiano.remove(converted_notes, symbols)
-
-mei_file = "CF-010.mei"
-mei_notes = mei.get_all_notes("../data/" + mei_file)
 
 dt = datetime.now()
 
-with open("../tests/trial.txt", "w") as fo:
-    fo.write(str(dt) + "\n")
-    fo.write("\n" + "mei_notes in " + mei_file + "\n")
-    fo.write(note.count(mei_notes) + "\n")
-    fo.write("\n" + mei_notes + "\n")
-    fo.write("\n" + "volpiano_notes from " + csvfile + " in folio: " + folio + "\n")
-    fo.write(note.count(extracted_notes) + "\n")
-    fo.write("\n" + extracted_notes + "\n")
+CSV_FILE = "sal-data.csv"
+DATA_PATH = "../data/"
+MEI_PATH = "../data/mei_files/"
+
+mei_files = os.listdir(MEI_PATH)
+
+for mei_file in mei_files:
+    if mei_file in (".DS_Store", "Icon?"):
+        continue
+
+    # Queries for Volpiano notes in corresponding mei_file
+    folio = os.path.splitext(mei_file)[0]
+    notes = volpiano.query(DATA_PATH + CSV_FILE, folio)
+    converted_notes = volpiano.convert(notes)
+
+    symbols = ['1', '3', '7','4','-', 'i',
+               '5', '6','2','I', 'w', 'W',
+               'x', 'X', 'y','Y', 'z', 'Z']
+
+    extracted_notes = volpiano.remove(converted_notes, symbols)
+
+    # Queries for all mei notes in mei_file
+    mei_notes = mei.get_all_notes(MEI_PATH + mei_file)
+
+    # Outputs file containing notes to compare and relevant meta data
+    with open("../tests/" + folio + "_compare.txt", "w") as fo:
+        fo.write(str(dt) + "\n")
+        fo.write("\n" + "mei_notes in " + mei_file + "\n")
+        fo.write(note.count(mei_notes) + "\n")
+        fo.write("\n" + mei_notes + "\n")
+        fo.write("\n" + "volpiano_notes from " + CSV_FILE + " in folio: " + folio + "\n")
+        fo.write(note.count(extracted_notes) + "\n")
+        fo.write("\n" + extracted_notes + "\n")
