78273b77d9d44a423a8634133ba17d069350cba1
Rivu Khoda <rivu.khoda@gmail.com>
2015-08-07 22:29:43 -0400

    New: matrix based scoring can be passed as a parameter

--- notes.py
+++ notes.py
@@ -2,15 +2,22 @@ from Bio import pairwise2
 from Bio.pairwise2 import format_alignment
 from Bio.SubsMat import MatrixInfo as matlist
 
-MATRIX = matlist.blosum62
 
-def local_align(seq1, seq2):
-    for a in pairwise2.align.localdx(seq1.upper(), seq2.upper(), MATRIX):
-        print format_alignment(*a)
+def local_align(seq1, seq2, matrix=None):
+    if matrix is not None:
+        for a in pairwise2.align.localdx(seq1.upper(), seq2.upper(), matrix):
+            print format_alignment(*a)
+    else:
+        for a in pairwise2.align.localxx(seq1, seq2):
+            print format_alignment(*a)
 
-def global_align(seq1, seq2):
-    for a in pairwise2.align.localms(seq1.upper(), seq2.upper() , 2, -1, -.5, -.1):
-        print format_alignment(*a)
+def global_align(seq1, seq2, matrix):
+    if matrix is not None:
+        for a in pairwise2.align.globaldx(seq1, seq2, matrix):
+            print format_alignment(*a)
+    else:
+        for a in pairwise2.align.globalms(seq1, seq2 , 2, -1, -.5, -.1):
+            print format_alignment(*a)
 
 def count(notes):
     return "number of notes: {}".format(len(notes))
