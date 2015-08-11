af3841221a28dbc1a4c9880b6113d7b00a0cbd91
Rivu Khoda <rivu.khoda@gmail.com>
2015-08-11 14:43:20 -0400

    Refactor: Functions in Volpiano module has been more generalized and ported to notes module

--- notes.py
+++ notes.py
@@ -19,5 +19,29 @@ def global_align(seq1, seq2, matrix=None):
         for a in pairwise2.align.globalms(seq1, seq2, 2, -1, -.5, -.1):
             print format_alignment(*a)
 
-def count(notes):
-    return "number of notes: {}".format(len(notes))
+# Counts the the number of characters in sequence
+def count(sequence):
+    return "number of notes: {}".format(len(sequence))
+
+# Maps characters in sequence to desired character 
+def convert(sequence, map):
+
+    new_sequence = ""
+    for s in sequence:
+        if s not in map: 
+            new_sequence += s
+        else:
+            new_sequence += map.get(s)
+
+    return new_sequence
+
+# Removes desired symbols from input sequence
+def remove(sequence, symbols):
+    to_remove = symbols
+
+    new_sequence = ""
+    for s in sequence:
+        if s not in to_remove:
+            new_sequence += s
+
+    return new_sequence
