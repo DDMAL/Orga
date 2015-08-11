bbe254630cd69c0f360577b24ed12c49d83335a8
Rivu Khoda <rivu.khoda@gmail.com>
2015-08-10 23:44:06 -0400

    WIP: Fixed print statements

--- scikit-bio.py
+++ scikit-bio.py
@@ -1,9 +1,12 @@
 import skbio
 from skbio.alignment import local_pairwise_align_protein
 from skbio.alignment import global_pairwise_align_protein
+from datetime import datetime
 
-fasta_file1 = raw_input("Please input the name of  file containing the query sequence")
-fasta_file2 = raw_input("Please input the name of file containing the reference sequence")
+dt = datetime.now()
+
+fasta_file1 = raw_input("Please input the name of  file containing the query sequence:\n")
+fasta_file2 = raw_input("Please input the name of file containing the reference sequence:\n")
 s1 = skbio.Sequence.read("../data/fasta_files/" + fasta_file1)
 s2 = skbio.Sequence.read("../data/fasta_files/" + fasta_file2)
 
@@ -11,10 +14,11 @@ gap_open_penalty = 11
 gap_extend_penalty = 1
 
 r = local_pairwise_align_protein(s1, s2, gap_open_penalty, gap_extend_penalty)
+print "local_alignment using Smith-Waterman\n"
 print r
 r.write("../results/SCIKIT-BIO/result_local.txt")
 print "\n"
 r = global_pairwise_align_protein(s1, s2, gap_open_penalty, gap_extend_penalty)
+print "global_alignment using Neeleman-Wunsch\n"
 print r
 r.write("../results/SCIKIT-BIO/result_global.txt")
-
