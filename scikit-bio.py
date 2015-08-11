a435e9eb1d2a304a2dc04637be988e0121fdfffa
Rivu Khoda <rivu.khoda@gmail.com>
2015-08-11 00:36:06 -0400

    WIP: Writes the time@ which the script was executed

--- scikit-bio.py
+++ scikit-bio.py
@@ -17,8 +17,14 @@ r = local_pairwise_align_protein(s1, s2, gap_open_penalty, gap_extend_penalty)
 print "local_alignment using Smith-Waterman\n"
 print r
 r.write("../results/SCIKIT-BIO/result_local.txt")
+with open( "../results/SCIKIT-BIO/result_local.txt", "a") as fo:
+    fo.write("\n" + str(dt) + "\n")
+
 print "\n"
+
 r = global_pairwise_align_protein(s1, s2, gap_open_penalty, gap_extend_penalty)
 print "global_alignment using Neeleman-Wunsch\n"
 print r
 r.write("../results/SCIKIT-BIO/result_global.txt")
+with open( "../results/SCIKIT-BIO/result_global.txt", "a") as fo:
+    fo.write("\n" + str(dt) + "\n")
