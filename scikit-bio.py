8caf49617c333cc655999680979675552ba4f22e
Rivu Khoda <rivu.khoda@gmail.com>
2015-08-11 02:13:49 -0400

    WIP: Prints alignment scores for sequences

--- scikit-bio.py
+++ scikit-bio.py
@@ -5,26 +5,33 @@ from datetime import datetime
 
 dt = datetime.now()
 
+# files containing sequences to be aligned are assigned
 fasta_file1 = raw_input("Please input the name of  file containing the query sequence:\n")
 fasta_file2 = raw_input("Please input the name of file containing the reference sequence:\n")
 s1 = skbio.Sequence.read("../data/fasta_files/" + fasta_file1)
 s2 = skbio.Sequence.read("../data/fasta_files/" + fasta_file2)
 
+# Alignment parameters are set
+# substitution matrix = {}
 gap_open_penalty = 11
 gap_extend_penalty = 1
 
+# Does local alignment using query and reference strings
 r = local_pairwise_align_protein(s1, s2, gap_open_penalty, gap_extend_penalty)
 print "local_alignment using Smith-Waterman\n"
 print r
 r.write("../results/SCIKIT-BIO/result_local.txt")
 with open( "../results/SCIKIT-BIO/result_local.txt", "a") as fo:
+    fo.write("\n" + "Score: "+ str(r.score()))
     fo.write("\n" + str(dt) + "\n")
 
 print "\n"
 
+# Does global alignment using query and reference strings
 r = global_pairwise_align_protein(s1, s2, gap_open_penalty, gap_extend_penalty)
 print "global_alignment using Neeleman-Wunsch\n"
 print r
 r.write("../results/SCIKIT-BIO/result_global.txt")
 with open( "../results/SCIKIT-BIO/result_global.txt", "a") as fo:
+    fo.write("\n" + "Score: "+ str(r.score()))
     fo.write("\n" + str(dt) + "\n")
