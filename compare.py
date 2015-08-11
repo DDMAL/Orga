6e454a30c30b878093a34c8dcf85fa28136caa31
Rivu Khoda <rivu.khoda@gmail.com>
2015-08-11 16:06:38 -0400

    New: Prints the previous and next folio pages from which the last and first chants were resepctively taken from.

--- compare.py
+++ compare.py
@@ -28,19 +28,23 @@ for mei_file in mei_files:
 
     # Queries for Volpiano notes in corresponding mei_file
     folio = os.path.splitext(mei_file)[0]
-    queried_notes = volpiano.query(CSV_PATH + CSV_FILE, folio)
-    converted_notes = notes.convert(queried_notes[0], VOLPIANO_MAP)
+    queried_notes = volpiano.query(os.path.join(CSV_PATH, CSV_FILE), folio)[0]
+    converted_notes = notes.convert(queried_notes, VOLPIANO_MAP)
     volpiano_notes = notes.remove(converted_notes, VOLPIANO_SYMBOLS)
 
     # Queries for all mei notes in mei_file
     mei_notes = mei.get_all_notes(MEI_PATH + mei_file)
 
     # Outputs file containing notes to compare and relevant meta data
+    previous_chant = volpiano.query(os.path.join(CSV_PATH, CSV_FILE), folio)[1]
+    next_chant = volpiano.query(os.path.join(CSV_PATH, CSV_FILE), folio)[2]
+
     with open("../results/COMPARE/" + folio + "_compare.txt", "w") as fo:
         fo.write(str(dt) + "\n")
         fo.write("\n" + "mei_notes in " + mei_file + "\n")
         fo.write(notes.count(mei_notes) + "\n")
         fo.write("\n" + mei_notes + "\n")
-        fo.write("\n" + "volpiano_notes from " + CSV_FILE + " in folio: " + folio + "\n")
-        fo.write(notes.count(volpiano_notes) + "\n")
+        fo.write("\n" + "volpiano_notes from " + CSV_FILE + " in folio: " + folio)
+        fo.write(" (previous_chant: " + previous_chant + ", next_chant: " + next_chant + ") ")
+        fo.write("\n" + notes.count(volpiano_notes) + "\n")
         fo.write("\n" + volpiano_notes + "\n")
