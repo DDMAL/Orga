ad94dedee00ad82a55b358ab140d6226acc6bf14
Rivu Khoda <rivu.khoda@gmail.com>
2015-08-15 00:51:18 -0400

    Refactor: Removed pairwise2 functions

--- notes.py
+++ notes.py
@@ -1,42 +1,53 @@
-from Bio import pairwise2
-from Bio.pairwise2 import format_alignment
-from Bio.SubsMat import MatrixInfo as matlist
 
 
-def local_align(seq1, seq2, matrix=None):
-    if matrix is not None:
-        for a in pairwise2.align.localds(seq1.upper(), seq2.upper(), matrix, -10, -.5):
-            print format_alignment(*a)
-    else:
-        for a in pairwise2.align.localms(seq1, seq2, 2, -1, -.5, -.1):
-            print format_alignment(*a)
-
-def global_align(seq1, seq2, matrix=None):
-    if matrix is not None:
-        for a in pairwise2.align.globalds(seq1.upper(), seq2.upper(), matrix, -10, -.5):
-            print format_alignment(*a)
-    else:
-        for a in pairwise2.align.globalms(seq1, seq2, 2, -1, -.5, -.1):
-            print format_alignment(*a)
-
-# Counts the the number of characters in sequence
 def count(sequence):
+    """Counts the the number of characters in sequence
+
+    Args:
+        sequence (str): input sequence
+
+    Returns:
+        number of characters in input sequence
+
+    """
+
     return "number of notes: {}".format(len(sequence))
 
-# Maps characters in sequence to desired character 
 def convert(sequence, map):
+
+    """Maps characters in sequence to desired character
+
+    Args:
+        sequence (str): input sequence
+        map (dict): dictionary of characters mapping to another character
+
+    Returns:
+        sequence with characters mapped to new characters according to input dictionary
+
+    """
 
     new_sequence = ""
     for s in sequence:
-        if s not in map: 
+        if s not in map:
             new_sequence += s
         else:
             new_sequence += map.get(s)
 
     return new_sequence
 
-# Removes desired symbols from input sequence
 def remove(sequence, symbols):
+
+    """Removes desired symbols from input sequence
+
+    Args:
+        sequence (str): input sequence
+        symbols (list): list of symbols that is to be removed from input sequence
+
+    Returns:
+        sequence without characters in list of symbols
+
+    """
+
     to_remove = symbols
 
     new_sequence = ""
@@ -45,3 +56,5 @@ def remove(sequence, symbols):
             new_sequence += s
 
     return new_sequence
+
+
