2d21481cbf8ed5063b248f61fe1a2869cf6cb502
Rivu Khoda <rivu.khoda@gmail.com>
2015-08-17 15:20:06 -0400

    Docs: Added missing return statment description

--- mei.py
+++ mei.py
@@ -2,12 +2,14 @@ import xml.etree.ElementTree as ET
 import os
 
 def get_all_notes(file_path):
-	"""Extracts all values assigned to pname attribute of neume ElementTree.
+    """Extracts all values assigned to pname attribute of neume ElementTree.
 
-	Args:
-		file_path (str): Path to file from which elements will be extracted.
+    Args:
+        file_path (str): Path to file from which elements will be extracted.
 
-	"""
+    Returns:
+        A str containing all the notes in selected mei file.
+    """
     script_dir = os.path.dirname(__file__)
     abs_file_path = os.path.join(script_dir, file_path)
 
