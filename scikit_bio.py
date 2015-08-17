c5a8e49e34918914b59c44fd7bef403c797468aa
Rivu Khoda <rivu.khoda@gmail.com>
2015-08-17 15:20:58 -0400

    Refactor: Constructed a more modular script

--- scikit_bio.py
+++ scikit_bio.py
@@ -1,46 +1,46 @@
-import skbio
 from skbio.alignment import local_pairwise_align_protein
 from skbio.alignment import global_pairwise_align_protein
 from datetime import datetime
-from skbio.alignment import StripedSmithWaterman
+
+
 dt = datetime.now()
 
-# files containing sequences to be aligned are assigned
-fasta_file1 = raw_input("Please input the name of  file containing the query sequence:\n")
-fasta_file2 = raw_input("Please input the name of file containing the reference sequence:\n")
-s1 = skbio.Sequence.read("../data/fasta_files/" + fasta_file1)
-s2 = skbio.Sequence.read("../data/fasta_files/" + fasta_file2)
+def skbio_local_align(seq1, seq2, gap_open_penalty, gap_extend_penalty, outfile):
+    """Local alignment using query and reference strings.
 
-# Alignment parameters are set
-# substitution matrix = {}
-gap_open_penalty = 11
-gap_extend_penalty = 1
+    Args:
+        seq1 (str): Query sequence.
+        seq2 (str): Subject sequence.
+        gap_open_penalty (int): Penalty for opening a gap.
+        gap_extend_penalty (int): Penalty for extending a gap.
 
-# Does local alignment using query and reference strings
-r = local_pairwise_align_protein(s1, s2, gap_open_penalty, gap_extend_penalty)
-print "local_alignment using Smith-Waterman\n"
-print r
-r.write("../results/SCIKIT-BIO/result_local.txt")
-with open( "../results/SCIKIT-BIO/result_local.txt", "a") as fo:
-    fo.write("\n" + "Score: "+ str(r.score()))
-    fo.write("\n" + str(dt) + "\n")
+    Returns:
+        File containing local alignment result of input sequences.
+    """
 
-print "\n"
+    r = local_pairwise_align_protein(seq1, seq2, gap_open_penalty, gap_extend_penalty)
+    r.write(outfile)
+    with open( outfile, "a") as fo:
+        fo.write("\n" + "Score: "+ str(r.score()))
+        fo.write("\n" + str(dt) + "\n")
 
-# Does global alignment using query and reference strings
-r = global_pairwise_align_protein(s1, s2, gap_open_penalty, gap_extend_penalty)
-print "global_alignment using Neeleman-Wunsch\n"
-print r
-r.write("../results/SCIKIT-BIO/result_global.txt")
-with open( "../results/SCIKIT-BIO/result_global.txt", "a") as fo:
-    fo.write("\n" + "Score: "+ str(r.score()))
-    fo.write("\n" + str(dt) + "\n")
+def skbio_global_align(seq1, seq2, gap_open_penalty, gap_extend_penalty, outfile):
+    """Global alignment using query and reference strings.
 
-# Does optimized local alignment using query and reference string
-print  'Optimized Alignment Algorithm (Striped Smith Waterman - Local Alignment)'
-query = StripedSmithWaterman(str(s1))
-alignment = query(str(s2))
-print alignment
-with open ("../results/SCIKIT-BIO/striped.txt", "a") as fo:
-    fo.write(str(alignment))
-    fo.write("\n" + str(dt) + "\n")
+        Args:
+            seq1 (str): Query sequence.
+            seq2 (str): Subject sequence.
+            gap_open_penalty (int): Penalty for opening a gap.
+            gap_extend_penalty (int): Penalty for extending a gap.
+
+        Returns:
+            File containing global alignment result of input sequences.
+
+    """
+    r = global_pairwise_align_protein(seq1, seq2, gap_open_penalty, gap_extend_penalty)
+    r.write(outfile)
+    with open( outfile, "a") as fo:
+        fo.write("\n" + "Score: "+ str(r.score()))
+        fo.write("\n" + str(dt) + "\n")
+
+
